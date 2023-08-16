bcac86e4d467038094a276052b16dc5f5fb6c778
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-08-16 19:16:49 -0300

    feat: add url changer

---
+++ background.js
@@ -0,0 +1,26 @@
+const VALUES = {
+  local: ':3000/',
+  aem: ':4502/editor.html/',
+};
+
+const replacedUrl = (url = '') => {
+  if (url.includes(VALUES.aem)) {
+    const replacedPort = url.replace(VALUES.aem, VALUES.local);
+    return replacedPort.replace(/(\.html)$/gmi, '');
+  }
+
+  const replacedPort = url.replace(VALUES.local, VALUES.aem);
+  return `${replacedPort}.html`;
+}
+
+chrome.action.onClicked.addListener(async () => {
+  const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (activeTab) {
+    const newUrl = replacedUrl(activeTab.url);
+
+    if (newUrl) {
+      chrome.tabs.update(activeTab.id, { url: newUrl });
+    }
+  }
+});
